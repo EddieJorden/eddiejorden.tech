464d00af49c121f5f837a22b63ad242f0dafb42f
EddieJorden <74274857+EddieJorden@users.noreply.github.com>
2021-01-04 22:41:37 -0800

    new base project

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div>
+    
+    <h1>How much is your time worth?</h1>
+  <img src="/images/lostaf.jpg" alt=""/>
+ </div>
+)
+
+export default HomePage
